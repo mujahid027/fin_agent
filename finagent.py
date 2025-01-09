7d9663dbc2c605cc7b5c1e5f8b46e040aa55a4c2
Mohammed Mujahid <116889207+mujahid027@users.noreply.github.com>
2025-01-09 21:15:24 +0530

    Create finagent.py

---
+++ finagent.py
@@ -0,0 +1,38 @@
+from phi.model.groq import Groq
+from phi.agent import Agent
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.yfinance import YFinanceTools
+from dotenv import load_dotenv
+
+# web search agent
+web_search_agent = Agent(
+    name ="web search agent",
+    role = "search the web for information",
+    model =  Groq(id = "Llama-3.3-70b-versatile",api_key="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"),
+    tools = [DuckDuckGo()],
+    instructions = ["always provide source"],
+    show_tools_calls= True,
+    markdown = True
+)
+
+#finacial agent
+financial_agent = Agent(
+    name = "financial agent",
+    model =  Groq(id = "Llama-3.3-70b-versatile",api_key="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"),
+    tools =[YFinanceTools(stock_price = True , analyst_recommendations = True ,
+                           stock_fundamentals = True ,company_news = True,technical_indicators = True)],
+    instructions = ["use table to show data"],
+    show_tools_calls = True,
+    markdown = True
+)
+
+#multi ai agent
+multi_ai_agent = Agent(
+    team = [web_search_agent,financial_agent],
+    model =  Groq(id = "Llama-3.3-70b-versatile",api_key="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"),
+    instructions=["always show sources","use tables to show data"],
+    show_tools_calls = True,
+    markdown = True
+)
+
+multi_ai_agent.print_response("Summarize analyst recommendations and share the latest news for NVDA", stream=True)
